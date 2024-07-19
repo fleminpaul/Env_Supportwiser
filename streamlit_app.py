8edcac1f1c3208019777560dd4fad7adbd72735e
Flemin Paul <Flemin Paul>
2024-07-19 19:27:28 +0530

    Client App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,106 @@
-import streamlit as st
+# streamlit_app.py
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import hmac
+import streamlit as st
+import streamlit as st
+import random
+import time
+import requests
+import json
+
+global username 
+assistant="supportwiser"
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            username = st.session_state["username"]
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
+# # Main Streamlit app starts here
+# st.write("Here goes your normal Streamlit app...")
+# st.button("Click me")
+
+def get_chatResponse():
+    payload = {'query': st.session_state.messages[-1]["content"]}
+    print(payload)
+    
+    # Send the request with JSON payload
+    response = requests.post(
+        "http://localhost:8080/ask_pdf",
+        json=payload,
+        headers={"content-type": "application/json"}
+    )
+    # Print the response for debugging
+    print(response.status_code)
+    print(response.text)
+    return response.json()["answer"]
+
+
+# Streamed response emulator
+def response_generator():
+    response = get_chatResponse()
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("SupportWiser")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role":"assistant", "content": response})
+
+
